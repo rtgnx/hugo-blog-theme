da8cfd96721942454e3f05715154f3a31d78d42f
Adrian Cybulski <adrian@rtgnx.xyz>
2017-07-22 14:54:10 +0100

    Remove copyfonts task

--- gulpfile.js
+++ gulpfile.js
@@ -5,35 +5,20 @@ const watch = require('gulp-watch')
 const webpack = require('gulp-webpack')
 const cssConcat = require('gulp-concat-css');
 
-
-
 gulp.task('css', function() {
-    return gulp.src('./assets/css/*.css')
-        .pipe(cssConcat('./app.css', { rebaseUrls: false}))
-        .pipe(gulp.dest('./static/css/'));
+  return gulp.src('./assets/css/*.css')
+    .pipe(cssConcat('./app.css', { rebaseUrls: false}))
+    .pipe(gulp.dest('./static/css/'));
 });
 
 
 gulp.task('js', function() {
-    return gulp.src('./assets/js/main.js')
-        .pipe(webpack({
-            output: {
-                filename: 'all.js',
-            }
-        })).pipe(gulp.dest('./static/js/'));
+  return gulp.src('./assets/js/main.js')
+    .pipe(webpack({
+      output: {
+        filename: 'all.js',
+      }
+    })).pipe(gulp.dest('./static/js/'));
 });
 
-gulp.task('default', ['css', 'js', 'copyfonts']);
-
-
-
-gulp.task('copyfonts', function() {
-    gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}')
-        .pipe(gulp.dest('./static/fonts/'));
-
-    gulp.src('./node_modules/bootstrap/fonts/*.{ttf,woff,woff2,svg}')
-        .pipe(gulp.dest('./static/fonts/bootstrap/'));
-
-    gulp.src('./node_modules/foundation-icons/*.{ttf,woff,woff2,svg}')
-        .pipe(gulp.dest('./static/fonts/'));
-});
+gulp.task('default', ['css', 'js']);
