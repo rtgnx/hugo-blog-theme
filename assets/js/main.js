b7e2418c50d69a7d2beb4814ee4a685ace2e7e49
Adrian Cybulski <adrian@rtgnx.xyz>
2017-07-22 12:52:21 +0100

    Remove boostrap require and sidebar code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,8 @@
 window._ = require('lodash');
 window.$ = window.jQuery = require('jquery');
-require('bootstrap-sass');
 
-require('./../../node_modules/bootstrap/dist/js/bootstrap.min.js'); 
+require('./../../node_modules/bootstrap/dist/js/bootstrap.min.js');
 var hljs = require('./../../static/js/highlight/highlight.pack.js');
-
-
-
-
-function extendSidebar(minWidth) {
-    if (window.screen.width > 768) {
-        $('#sidebar').css('min-height', minWidth);
-    }
-}
 
 $(document).ready(function () {
     extendSidebar(
