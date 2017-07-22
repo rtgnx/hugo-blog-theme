a107c059d0ead58513fbe5db41e5493f0522b50c
Adrian Cybulski <adrian@rtgnx.xyz>
2017-07-22 12:57:51 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 window._ = require('lodash');
 window.$ = window.jQuery = require('jquery');
 
-require('./../../node_modules/bootstrap/dist/js/bootstrap.min.js');
+
 var hljs = require('./../../static/js/highlight/highlight.pack.js');
 
 $(document).ready(function () {
